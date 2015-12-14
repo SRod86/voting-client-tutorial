0c82bf8d186bc29ca5c3ca8f124378633868afde
Steven Rodriguez <sfrodriguez86@gmail.com>
2015-12-14 16:11:58 -0700

    Add results component & spec

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import Router, {Route} from 'react-router';
 import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
 const routes = <Route component={App}>
   <Route path="/results" component={Results} />
   <Route path="/" component={Voting} />
-}
 </Route>;
 
 ReactDOM.render(
